689d593e66e9d4392ee4de2c26c814b9aacf597d
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-03-12 15:44:24 -0600

    app init

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql').graphqlHTTP;
-const schema = require('./schema.js');
 
 const app = express();
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
-);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(4000, () => {
+  console.log('Server is running');
+});
